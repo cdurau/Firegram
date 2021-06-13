73f8a7d791aa6367c72158345d45621053e204f9
cdurau <{ID}+{username}@users.noreply.github.com>
2021-06-14 01:57:27 +0200

    + Added UploadForm

---
+++ src/components/UploadForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const types = ['image/png', 'image/jpeg'];
+
+    const changeHandler = e => {
+        let selected = e.target.files[0];
+
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError(null);
+        } else {
+            setFile(null);
+            setError('Please select an image file (jpeg or png)');
+        }
+    };
+
+    return (
+        <form>
+            <input type='file' onChange={changeHandler} />
+            <div className='output'>
+                {file && <div className='file'>{file.name}</div>}
+                {error && <div className='error'>{error}</div>}
+            </div>
+        </form>
+    );
+};
+
+export default UploadForm;
